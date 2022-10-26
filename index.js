9ffe07442a973ff626bb0cc9d394504b6fe9a15e
Avinash DV <avinash.dv.264@gmail.com>
2022-10-26 10:23:03 +0530

    initial commit nft-preview

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //   const qrOne = document.getElementById("qrComponent__transitionOne");
+  //   const qrTwo = document.getElementById("qrComponent__transitionTwo");
+  //   const qrThree = document.getElementById("qrComponent__transitionThree");
+  //   const qrFour = document.getElementById("qrComponent__transitionFour");
+  //   //   console.log("///", qrr);
+  //   qrTwo.classList.add("qrComponent__transition");
+  //   qrThree.classList.add("qrComponent__transition");
+  //   qrFour.classList.add("qrComponent__transition");
+});
